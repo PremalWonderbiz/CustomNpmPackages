3d769a6a0f0cde1e8f895df52fce33e09f4f4a5f
PremalWonderbiz <premal.kadam@wonderbiz.in>
2025-10-23 12:05:47 +0530

    first commit with 2 simple npm packages

---
+++ simple-calculator/index.js
@@ -0,0 +1,14 @@
+export const add = function (a, b) {
+    return a + b;
+};
+export const sub = function (a, b) {
+    return a - b;
+};
+export const mul = function (a, b) {
+    return a * b;
+};
+export const div = function (a, b) {
+    return a / b;
+};
+console.log(mul(2, 3));
+//# sourceMappingURL=index.js.map
---
+++ simple-calculator/index.ts
@@ -0,0 +1,17 @@
+export const add = function (a : number, b : number) : number {
+    return a + b;
+}
+
+export const sub = function (a : number, b : number) : number {
+    return a - b;
+}
+
+export const mul = function (a : number, b : number) : number {
+    return a * b;
+}
+
+export const div = function (a : number, b : number) : number {
+    return a / b;
+}
+
+console.log(mul(2,3));
